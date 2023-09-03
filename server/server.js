fe2f4827bac9d18011b68a2f8b336a024886e1fb
Paul HoangLong <paulhoanglong@gmail.com>
2023-09-02 19:38:36 -0500

    Already had list reversed, but redid it using request instead of querytext

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 const tasksRouter = require('./routes/tasks.router')
 
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
 
